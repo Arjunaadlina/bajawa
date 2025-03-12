8ed670040cc0e45de1983c5a23a3fb852efbe73f
Arjunaadlina <arjunaadlinamartha@gmail.com>
2025-03-13 05:09:22 +0700

    add learning card

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { useAuth } from '../../context/auth';
 import { QuickSand } from '@/components/Text/QuickSand';
+import LearningCard from '@/components/Card/LearningCard';
 
 export default function Home() {
     const { logout } = useAuth();
@@ -8,22 +9,47 @@ export default function Home() {
     return (
         <ScrollView className="flex-1 bg-primary">
             <View className="p-6">
-                <View className='flex-row items-center mt-16 gap-4 border-2'>
+                <View className='flex-row items-center mt-16 gap-4 '>
                     <Image
-                        source={require('../../assets/images/bajawa.png')}
+                        source={require('../../assets/images/profile.jpeg')}
                         className="rounded-full"
                         style={{ width: 100, height: 100 }}
                     />
-                    <View className='justify-center border-2'>
-                        <QuickSand weight='semibold' className='text-white text-[1.3rem]'>Hi, Michaela 👋</QuickSand>
+                    <View className='justify-center '>
+                        <QuickSand weight='semibold' className='text-white text-[1.6rem]'>Hi, Michaela 👋</QuickSand>
                         <QuickSand className='text-white text-sm' >Ayo mulai belajar!</QuickSand>
                     </View>
                 </View>
 
                 <View className='flex-row justify-between mt-8 w-full ' >
-                    <View className='bg-white rounded-lg' style={{width: 150, height:60}}/>
-                    <View className='bg-white rounded-lg' style={{width: 150, height:60}}/>
+                    <View className='bg-white rounded-lg' style={{width: 160, height:65}}/>
+                    <View className='bg-white rounded-lg' style={{width: 160, height:65}}/>
                 </View>
+            </View>
+
+            <View className='bg-bgPrimary h-[200%] mt-4 rounded-t-[3rem] pt-6 px-6'>
+                <LearningCard
+                    title='Aksara Jawa'
+                    bgColor='bg-yellow-300'
+                    progressText='Lanjutkan pembelajaran ke-2'
+                    description='Terdapat 6 pembelajaran'
+                    imageSrc={require('../../assets/images/aksara.png')}
+                />
+                <LearningCard
+                    title='Tingkatan Bahasa'
+                    bgColor='bg-[#FBA6F8]'
+                    progressText='Lanjutkan pembelajaran ke-4'
+                    description='Terdapat 10 pembelajaran'
+                    imageSrc={require('../../assets/images/tingkatan.png')}
+                />
+                <LearningCard
+                    title='Kata Kerja'
+                    bgColor='bg-[#78EEE9]'
+                    progressText='Mulai pembelajaran'
+                    description='Terdapat 7 pembelajaran'
+                    colorProggress='bg-[#FDFFB7]'
+                    imageSrc={require('../../assets/images/katakerja.png')}
+                />
             </View>
         </ScrollView>
     );
---
+++ components/Card/LearningCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Image, ImageSourcePropType } from "react-native";
+import { QuickSand } from "../Text/QuickSand";
+
+interface LearningCardProps {
+    title: string;
+    progressText: string;
+    description: string;
+    bgColor?: string;
+    colorProggress?: string;
+    imageSrc: ImageSourcePropType;
+}
+
+const LearningCard: React.FC<LearningCardProps> = ({
+    title,
+    progressText,
+    description,
+    bgColor = "bg-yellow-300",
+    colorProggress = "bg-green-200",
+    imageSrc,
+    }) => {
+    return (
+        <View className="bg-white h-[9%] rounded-[2rem] items-center px-4 flex-row gap-4 mb-4">
+            <View className={`${bgColor} h-24 w-24 rounded-xl justify-center items-center`}>
+                <Image source={imageSrc} />
+            </View>
+            <View>
+                <QuickSand weight="bold" className="text-xl">{title}</QuickSand>
+                <View className={`${colorProggress} p-[0.2rem] rounded-lg self-start`}>
+                    <QuickSand className="text-xs">{progressText}</QuickSand>
+                </View>
+                <QuickSand className="text-gray-500 mt-4 font-semibold">{description}</QuickSand>
+            </View>
+        </View>
+    );
+};
+
+export default LearningCard;
